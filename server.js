4f6980ee5a8ca037876725ac0be93b5e56a6fd42
Lorenzo Di Donna <dilorenzo@itis-molinari.eu>
2025-04-07 11:43:40 +0200

    schema er , api trovato

---
+++ server.js
@@ -0,0 +1,6 @@
+/*
+chiamata api 
+/api/explore/v2.1/catalog/datasets/all-vehicles-model/records?select=*&where=model%3D%22Yaris%22%20and%20fueltype%3D%22Regular%22&limit=20
+sito 
+https://public.opendatasoft.com/explore/dataset/all-vehicles-model/api/?sort=modifiedon
+*/
