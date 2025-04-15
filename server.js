aa4cda67aad97ca6dbf4294134c3a953588f1bc2
Simone Barani <baranisimone@itis-molinari.eu>
2025-04-15 13:37:42 +0200

    inizio coding

--- server.js
+++ server.js
@@ -4,3 +4,47 @@ chiamata api
 sito 
 https://public.opendatasoft.com/explore/dataset/all-vehicles-model/api/?sort=modifiedon
 */
+
+const bodyParser = require('body-parser');
+const express = require("express");
+const http = require('http');
+const path = require('path');
+const app = express();
+const database = require("./database");
+database.createTables();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+
+app.post("/insert", async (req, res) => {
+    const annuncio = req.body;
+    try {
+      await database.insert(annuncio);
+      res.json({result: "ok"});
+    } catch (e) {
+      console.error(e)
+      res.status(500).json({result: "ko"});
+    }
+  })
+  app.get('/bookings', async (req, res) => {
+      const list = await database.selectAnnuncio();
+      res.json(list);
+  });
+  app.delete('/delete/:id', async (req, res) => {
+    await database.delete(req.params.id);
+    res.json({result: "ok"});
+  })
+  const server = http.createServer(app);
+  const port = 5600;
+  server.listen(port, () => {
+    console.log("- server running on port: " + port);
+  });
+  
+  app.get('/types', async (req, res) => {
+    const list = await database.selectTypes();
+    res.json(list);
+  });
