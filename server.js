f9d7e0520dd8694def707861a7eb3ef22323fc02
Lorenzo <dilorenzo@itis-molinari.eu>
2025-04-29 13:21:48 +0200

    filtri

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use("/", express.static(path.join(__dirname, "public")));
 //app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+
 /*
 app.post("/insert", async (req, res) => {
     const annuncio = req.body;
